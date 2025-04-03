8287d51aaf34d6794ef81cca9001db2b09fb19e9
Ethan Chen <echen@lcs.on.ca>
2025-04-03 19:39:45 -0400

    Initial Commit

---
+++ TodoListRemake/TodoListRemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListRemakeApp.swift
+//  TodoListRemake
+//
+//  Created by Pak Ching Ethan Chen on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct TodoListRemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
